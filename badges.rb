11f8c8876cd6f720dbbb3d6d0deaac1255c4eaa2
Ila Leigh Baugham <leigh.baugham@gmail.com>
2018-11-04 20:49:21 +0000

    automatically backed up by learn

---
+++ badges.rb
@@ -0,0 +1,14 @@
+def badge_maker(name="Arel")
+  puts "Hello, my name is " + name + "." 
+end
+
+
+
+
+names = ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
+
+names.each do |name|
+puts “ Hello, my name is #{name}”
+end
+
+
---
+++ practice.rb
@@ -0,0 +1,20 @@
+x = 1 
+100.times do
+  puts x = x * 2
+end
+
+
+5.times do |time|
+  if 
+  # puts "hi"
+  puts n
+  # puts "odd"
+  
+  
+  names = [ "rob", "emily", "sarah"]
+  
+  greets =[]
+  names.each do |names|
+    greetings << "Hi, #{name}"
+  end
+  print greetings
